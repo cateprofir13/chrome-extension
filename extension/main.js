52d2578f098e842f2323c4ce2b5a57326bc189a3
Ecaterina Profir <ecaterina.profir@gmail.com>
2025-05-21 20:27:21 -0500

    work in progress

--- extension/main.js
+++ extension/main.js
@@ -1,9 +1,15 @@
-let selection = window.getSelection()
+//   let selection = window.getSelection().toString()
+
+currWindow = chrome.tabs.query({active:true, currentWindow:true})
+
 body = document.querySelector('body')
 button = document.createElement('button')
 button.setAttribute('id', 'button')
 body.appendChild(button)
 
 button.onclick = () => {
-  console.log('Hello')
+  console.log(currWindow.getSelection().toString())
+  
 }
+
+
